a9eb0d79039700da56819d3341bfcea56466f9fa
Indy <dev@indydedeken.fr>
2014-10-02 15:04:02 +0200

    Résultats cURL
    La librairie PHP cURL fonctionne.

--- src/test-good-model.php
+++ src/test-good-model.php
@@ -4,8 +4,19 @@ require '../vendor/autoload.php';
 use \Curl\Curl;
 
 $curl = new Curl();
-$curl->get('http://5.196.11.220:5232/calendar/',
-    array('param1' => 'value1', 'param2' => 'value2')
-);
+$curl->head('http://www.indydedeken.fr');
 
-// voir comment on rÃ©cupÃ¨re le retour...
+if(!$curl->curl_error) {
+    if($curl->http_status_code == 200) {
+        echo "La requête est bien arrivée";
+    } else {
+        echo "La requête a échoué";
+    }
+} else {
+    echo "cURL non lancée";
+}
+
+echo "<br><hr><br>";
+
+// affiche le contenu de la réponse cURL
+var_dump($curl);
