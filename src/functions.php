fcae89b1b90479ebcad5f368ac3fa15339dbe95f
Indy <dev@indydedeken.fr>
2014-10-05 11:45:06 +0200

    Retour des méthodes Caldav
    CF RFC4791 : http://tools.ietf.org/html/rfc4791

---
+++ src/functions.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @param $curl
+ * @param array $httpStatus
+ * @return array
+ */
+function publishBehavior($curl, $httpStatus = array(200)) {
+
+    $behavior = array();
+
+    // requete
+    if(!$curl->curl_error) {
+        if( in_array($curl->http_status_code, $httpStatus) ) {
+            $behavior['requete'] = "La requête est bien arrivée";
+        } else {
+            $behavior['requete'] = "La requête a échoué";
+        }
+    } else {
+        $behavior['requete'] = "cURL KO";
+    }
+
+    // contenu de la requete
+    if($behavior['requete'] != "cURL KO") {
+
+        $behavior['contenu'] = "<pre>";
+        $behavior['contenu'] .= print_r($curl, true);
+        $behavior['contenu'] .= "</pre>";
+
+    }
+
+    return $behavior;
+}
--- src/test-good-model.php
+++ src/test-good-model.php
@@ -1,24 +1,86 @@
 <?php
 require '../vendor/autoload.php';
+require_once 'functions.php';
 
 use \Curl\Curl;
 
+//
+// Premier objet cURL : OPTIONS
+//
+$curl = new Curl();
+$curl->options('http://compri.me:5232/radicale/');
+$data = array(200); // http status
+
+$test1 = publishBehavior($curl, $data);
+echo "<h1>OBJET 1 : OPTIONS</h1>";
+echo $test1['requete'];
+echo $test1['contenu'];
+//
+// Fin premier objet cURL : OPTIONS
+//
+unset($curl);
+?>
+
+<hr>
+
+<?php
+//
+// Second objet cURL : GET
+//
+$curl = new Curl();
+$curl->get('http://compri.me:5232/radicale/');
+$data = array(200, 201, 207);
+
+$test2 = publishBehavior($curl, $data);
+echo "<h1>OBJET 2 : GET</h1>";
+echo $test2['requete'];
+echo $test2['contenu'];
+//
+// Fin second objet cURL : GET
+//
+unset($curl);
+?>
+
+<hr>
+
+<?php
+//
+// Second objet cURL : HEAD
+//
+$curl = new Curl();
+$curl->head('http://compri.me:5232/radicale/');
+$data = array(200);
+
+$test3 = publishBehavior($curl, $data);
+echo "<h1>OBJET 3 : HEAD</h1>";
+echo $test3['requete'];
+echo $test3['contenu'];
+//
+// Fin second objet cURL : HEAD
+//
+unset($curl);
+?>
+
+<hr>
+
+<?php
+//
+// Second objet cURL : PROPFIND
+//
 $curl = new Curl();
 $curl->propfind('http://compri.me:5232/radicale/');
+$data = array(200, 207);
+
+$test4 = publishBehavior($curl, $data);
+echo "<h1>OBJET 3 : PROPFIND</h1>";
+echo $test4['requete'];
+echo $test4['contenu'];
+//
+// Fin second objet cURL : PROPFIND
+//
+unset($curl);
+?>
 
 
-
-if(!$curl->curl_error) {
-    if( in_array($curl->http_status_code, array(200, 207)) ) {
-        echo "La requête est bien arrivée";
-    } else {
-        echo "La requête a échoué";
-    }
-} else {
-    echo "cURL non lancée";
-}
-
-echo "<br><hr><br>";
-
-// affiche le contenu de la réponse cURL
-var_dump($curl);
+<?php
+// NE PAS FERMER LA BALISE PHP : <?php
