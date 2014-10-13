bea2032ed827460809a1b0717019f1843697d016
Nicolas Marechal <nicolasmar91@gmail.com>
2014-10-13 11:13:42 +0200

    Ajout de la fonctionnalité HTTP PUT aux fichier concernés

--- edt/icsetudiant/icsgroupe.php
+++ edt/icsetudiant/icsgroupe.php
@@ -4,6 +4,9 @@ include("../config.php");
 mysql_connect($serveur,$user,$pass);
 mysql_select_db($dernierebase);
 
+//CALDav project - START ------
+include("../CalDAV/CALDavCommunication.php");
+//CALDav project - FIN --------
 
 $jour=date('d');
 $mois=date('m');
@@ -282,15 +285,19 @@ while ($reservation_groupe = mysql_fetch_array($reservations_groupes) )
 		
 
 		
-		
-	$fichier.="END:VCALENDAR";
+			
+		$fichier.="END:VCALENDAR";
 
-	$nomfichier=$groupe['nom'].".ics";
-	//$nomfichier=ereg_replace("[ ]","_",$nomfichier);
-$nomfichier=preg_replace('/\s/',"_",$nomfichier);
-	$nomfichier=strtolower($nomfichier);
-	file_put_contents($nomfichier,$fichier);
+		$nomfichier=$groupe['nom'].".ics";
+		//$nomfichier=ereg_replace("[ ]","_",$nomfichier);
+		$nomfichier=preg_replace('/\s/',"_",$nomfichier);
+		$nomfichier=strtolower($nomfichier);
+		file_put_contents($nomfichier,$fichier);
 	
+		//CALDav project - START ------
+		$uid = $annee.$mois.$jour."T"."000001Z-".$i."@ufrsitec.u-paris10.fr";
+		sendICSFile($nomfichier,$fichier,$ENSEIGNANT,$uid);
+		//---------------- FIN --------
 	
 	}
 
