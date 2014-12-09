b163fcc287af79aefd5df0f33639c244164b8688
Indy <dev@indydedeken.fr>
2014-12-10 00:46:48 +0100

    Amélioration icsSalle
    amélioration du temps de génération de l’ics
    correction des erreurs d’indentation

--- Site/ics/icssalle/icssalle.php
+++ Site/ics/icssalle/icssalle.php
@@ -11,29 +11,29 @@ include("../../script/CalDAVCommunication.php");
 
 date_default_timezone_set('Europe/Paris');
 setlocale(LC_TIME, 'fr_FR');
-$jour	=date('d');
-$mois 	=date('m');
-$annee	=date('Y');
-$heure	=date('H');
-$minute	=date('i');
+$jour	= date('d');
+$mois 	= date('m');
+$annee	= date('Y');
+$heure	= date('H');
+$minute	= date('i');
 $i=0;
 $newline = "\n";
 
 // Pour générer un calendrier de salle précis
 // recuperation de : ID NOM
 if(	isset($_POST['idsalle'])
-	&& !empty($_POST['idsalle'])
-	&& isset($_POST['nom'])
-	&& !empty($_POST['nom'])
-	// user == prof logged
+&& !empty($_POST['idsalle'])
+&& isset($_POST['nom'])
+&& !empty($_POST['nom'])
+// user == prof logged
 ) {
-		$idSalle	= htmlspecialchars($_POST['idsalle'], ENT_QUOTES, 'UTF-8');
-		$nomSalle	= htmlspecialchars($_POST['nom'], ENT_QUOTES, 'UTF-8');
+	$idSalle	= htmlspecialchars($_POST['idsalle'], ENT_QUOTES, 'UTF-8');
+	$nomSalle	= htmlspecialchars($_POST['nom'], ENT_QUOTES, 'UTF-8');
 
-		$requete_precision_salle = "AND codeSalle=$idSalle AND nom='$nomSalle'";
+	$requete_precision_salle = "AND codeSalle=$idSalle AND nom='$nomSalle'";
 } else {
-		// si aucun prof n'est précisé => EXPORT TOUS CALENDRIERS
-		$requete_precision_salle = "";
+	// si aucun prof n'est précisé => EXPORT TOUS CALENDRIERS
+	$requete_precision_salle = "";
 }
 
 //constitution de la requête
@@ -77,23 +77,22 @@ foreach($dbh->query($ressources_salles) as $salle) {
 			$dateseance=preg_replace('/-/s',"",$dateseance);
 			$heuredebutseance=$seance['heureSeance'];
 			if (strlen($heuredebutseance)<=3)
-				{
-				$heuredebutseance="0".$heuredebutseance;
-				}
-			$anneeseance=substr($dateseance,0,4);
-			$moisseance=substr($dateseance,4,2);
-			$jourseance=substr($dateseance,6,2);
-			$heureseance=substr($heuredebutseance,0,2);
-			$minuteseance=substr($heuredebutseance,2,2);
+			$heuredebutseance="0".$heuredebutseance;
+
+			$anneeseance  = substr($dateseance,0,4);
+			$moisseance	  = substr($dateseance,4,2);
+			$jourseance	  = substr($dateseance,6,2);
+			$heureseance  = substr($heuredebutseance,0,2);
+			$minuteseance = substr($heuredebutseance,2,2);
 
 			$dates = gmstrftime("DTSTART:%Y%m%dT%H%M%SZ", mktime($heureseance, $minuteseance, 0, $moisseance, $jourseance, $anneeseance));
 
 			$fichier .= $dates.$newline;
 
 			//date fin seance
-			$heuredebut=gmstrftime("%H", mktime($heureseance, $minuteseance, 0, $moisseance, $jourseance, $anneeseance));
-			$mindebut=gmstrftime("%M", mktime($heureseance, $minuteseance, 0, $moisseance, $jourseance, $anneeseance));
-			$heuredebutenmin=$heuredebut*60+$mindebut;
+			$heuredebut	= gmstrftime("%H", mktime($heureseance, $minuteseance, 0, $moisseance, $jourseance, $anneeseance));
+			$mindebut	= gmstrftime("%M", mktime($heureseance, $minuteseance, 0, $moisseance, $jourseance, $anneeseance));
+			$heuredebutenmin = $heuredebut*60 + $mindebut;
 
 			if (strlen($seance['dureeSeance'])==4) {
 				$heureduree=substr($seance['dureeSeance'],0,2);
@@ -111,10 +110,10 @@ foreach($dbh->query($ressources_salles) as $salle) {
 			$heurefin=intval($heurefinenmin/60);
 
 			if (strlen($heurefin)==1)
-				$heurefin="0".$heurefin;
+			$heurefin="0".$heurefin;
 			$minfin=$heurefinenmin%60;
 			if (strlen($minfin)==1)
-				$minfin="0".$minfin;
+			$minfin="0".$minfin;
 
 			$fichier .= "DTEND:".$dateseance."T".$heurefin.$minfin."00Z".$newline;
 
@@ -134,19 +133,19 @@ foreach($dbh->query($ressources_salles) as $salle) {
 			if(!empty($seance['commentaire'])) {
 				$commentaire = utf8_encode($seance['commentaire']);
 				$commentaire = str_replace(
-										array('à', 'â', 'ä', 'á', 'ã', 'å',
-											'î', 'ï', 'ì', 'í',
-											'ô', 'ö', 'ò', 'ó', 'õ', 'ø',
-											'ù', 'û', 'ü', 'ú',
-											'é', 'è', 'ê', 'ë',
-											'ç', 'ÿ', 'ñ'),
-										array('a', 'a', 'a', 'a', 'a', 'a',
-											'i', 'i', 'i', 'i',
-											'o', 'o', 'o', 'o', 'o', 'o',
-											'u', 'u', 'u', 'u',
-											'e', 'e', 'e', 'e',
-											'c', 'y', 'n'),
-										$commentaire);
+				array('à', 'â', 'ä', 'á', 'ã', 'å',
+				'î', 'ï', 'ì', 'í',
+				'ô', 'ö', 'ò', 'ó', 'õ', 'ø',
+				'ù', 'û', 'ü', 'ú',
+				'é', 'è', 'ê', 'ë',
+				'ç', 'ÿ', 'ñ'),
+				array('a', 'a', 'a', 'a', 'a', 'a',
+				'i', 'i', 'i', 'i',
+				'o', 'o', 'o', 'o', 'o', 'o',
+				'u', 'u', 'u', 'u',
+				'e', 'e', 'e', 'e',
+				'c', 'y', 'n'),
+				$commentaire);
 				$commentaire = strtoupper($commentaire);
 				$fichier .= "DESCRIPTION;LANGUAGE=fr-CA:MATIERE : ".$cursename[1]." - ".$type."\nGROUPE : ".$nomgroupe."\nDUREE : ".$heureduree."h".$minduree. "\nCOMMENTAIRE : ".$commentaire. $newline;
 			}
@@ -154,123 +153,123 @@ foreach($dbh->query($ressources_salles) as $salle) {
 				$fichier .= "DESCRIPTION;LANGUAGE=fr-CA:MATIERE : ".$cursename[1]." - ".$type."\nGROUPE : ".$nomgroupe."\nDUREE : ".$heureduree."h".$minduree.$newline;
 			}
 
-		$fichier .= "DTSTAMP:".$annee.$mois.$jour."T".$heure.$minute."00Z". $newline;
-		$fichier .= "UID:".$annee.$mois.$jour."T"."000001Z-".$i."@ufrsitec.u-paris10.fr". $newline;
-		$i = $i+1;
-		$fichier .= "CATEGORIES:Emplois du temps du PST". $newline;
-
-		$fichier .= "END:VEVENT". $newline;
-	}
-}
-
-
-//reservations
-$reservations_salles=mysql_query("SELECT * FROM reservations_salles WHERE codeRessource='$salle[codeSalle]' AND deleted= '0'");
-while ($reservation_salle = mysql_fetch_array($reservations_salles) )
-		{
-			$reservations=mysql_query("SELECT * FROM reservations WHERE codeReservation='$reservation_salle[codeReservation]' AND deleted= '0'");
-
-
-
-			while ($reservation = mysql_fetch_array($reservations) )
-				{
-				$fichier .= "BEGIN:VEVENT". $newline;
-
-
-
-				//nom de la reservation
-				$commentaire=utf8_encode($reservation['commentaire']);
-				$commentaire=str_replace(array('à', 'â', 'ä', 'á', 'ã', 'å','î', 'ï', 'ì', 'í', 'ô', 'ö', 'ò', 'ó', 'õ', 'ø','ù', 'û', 'ü', 'ú','é', 'è', 'ê', 'ë','ç', 'ÿ', 'ñ'),array('a', 'a', 'a', 'a', 'a', 'a','i', 'i', 'i', 'i','o', 'o', 'o', 'o', 'o', 'o','u', 'u', 'u', 'u','e', 'e', 'e', 'e','c', 'y', 'n'),$commentaire);
-				$commentaire=strtoupper($commentaire);
-				$fichier .= "SUMMARY:".$commentaire.$newline;
-
-				$fichier .= "CLASS:PUBLIC". $newline;
-
-				//date debut reservation
-				$datereservation=$reservation['dateReservation'];
-				//$datereservation=ereg_replace("[-:]","",$datereservation);
-				$datereservation=preg_replace('/-/s',"",$datereservation);
-				$heuredebutreservation=$reservation['heureReservation'];
-				if (strlen($heuredebutreservation)<=3)
-					{
-					$heuredebutreservation="0".$heuredebutreservation;
-					}
-				$anneereservation=substr($datereservation,0,4);
-				$moisreservation=substr($datereservation,4,2);
-				$jourreservation=substr($datereservation,6,2);
-				$heurereservation=substr($heuredebutreservation,0,2);
-				$minutereservation=substr($heuredebutreservation,2,2);
-
-				$dates= gmstrftime("DTSTART:%Y%m%dT%H%M%SZ", mktime($heurereservation, $minutereservation, 0, $moisreservation, $jourreservation, $anneereservation));
-
-				$fichier .= $dates.$newline;
-
-				//date fin reservation
-				$heuredebut=gmstrftime("%H", mktime($heurereservation, $minutereservation, 0, $moisreservation, $jourreservation, $anneereservation));
-				$mindebut=gmstrftime("%M", mktime($heurereservation, $minutereservation, 0, $moisreservation, $jourreservation, $anneereservation));
-				$heuredebutenmin=$heuredebut*60+$mindebut;
-
-				if (strlen($reservation['dureeReservation'])==4)
-					{
-						$heureduree=substr($reservation['dureeReservation'],0,2);
-						$minduree=substr($reservation['dureeReservation'],2,2);
-					}
-				if (strlen($reservation['dureeReservation'])==3)
-					{
-						$heureduree=substr($reservation['dureeReservation'],0,1);
-						$minduree=substr($reservation['dureeReservation'],1,2);
-
-					}
-				if (strlen($reservation['dureeReservation'])==2)
-					{
-						$heureduree=0;
-						$minduree=$reservation['dureeReservation'];
-					}
-				$heurefinenmin=$heuredebutenmin+$heureduree*60+$minduree;
-				$heurefin=intval($heurefinenmin/60);
-
-				if (strlen($heurefin)==1)
-					{
-						$heurefin="0".$heurefin;
-					}
-				$minfin=$heurefinenmin%60;
-				if (strlen($minfin)==1)
-					{
-						$minfin="0".$minfin;
-					}
-				$fichier .= "DTEND:".$datereservation."T".$heurefin.$minfin."00Z".$newline;
-
-
-				//detail de la seance
-				$fichier .= "DESCRIPTION;LANGUAGE=fr-CA:INTITULE : ".$commentaire." \nDUREE : ".$heureduree."h".$minduree.$newline;
-
-
-
-			$fichier .= "CATEGORIES:Emplois du temps du PST". $newline;
 			$fichier .= "DTSTAMP:".$annee.$mois.$jour."T".$heure.$minute."00Z". $newline;
 			$fichier .= "UID:".$annee.$mois.$jour."T"."000001Z-".$i."@ufrsitec.u-paris10.fr". $newline;
-			$i=$i+1;
+			$i = $i+1;
+			$fichier .= "CATEGORIES:Emplois du temps du PST". $newline;
+
 			$fichier .= "END:VEVENT". $newline;
+		}
+	}
+
+	//reservations
+	$reservations_salles = "SELECT * FROM reservations_salles WHERE codeRessource='$salle[codeSalle]' AND deleted=0";
+
+	foreach($dbh->query($reservations_salles) as $reservation_salle) {
+		$reservations = "SELECT * FROM reservations WHERE codeReservation='$reservation_salle[codeReservation]' AND deleted=0";
+
+		foreach($dbh->query($reservations) as $reservation) {
+			$fichier .= "BEGIN:VEVENT". $newline;
+
+			//nom de la reservation
+			$commentaire = utf8_encode($reservation['commentaire']);
+			$commentaire = str_replace(
+			array('à', 'â', 'ä', 'á', 'ã', 'å',
+			'î', 'ï', 'ì', 'í',
+			'ô', 'ö', 'ò', 'ó', 'õ', 'ø',
+			'ù', 'û', 'ü', 'ú',
+			'é', 'è', 'ê', 'ë',
+			'ç', 'ÿ', 'ñ'),
+			array('a', 'a', 'a', 'a', 'a', 'a',
+			'i', 'i', 'i', 'i',
+			'o', 'o', 'o', 'o', 'o', 'o',
+			'u', 'u', 'u', 'u',
+			'e', 'e', 'e', 'e',
+			'c', 'y', 'n'),
+			$commentaire);
+
+			$commentaire = strtoupper($commentaire);
+
+			$fichier .= "SUMMARY:" . $commentaire . $newline;
+			$fichier .= "CLASS:PUBLIC" . $newline;
+
+			//date debut reservation
+			$datereservation = $reservation['dateReservation'];
+			$datereservation = preg_replace('/-/s',"", $datereservation);
+			$heuredebutreservation = $reservation['heureReservation'];
+			if (strlen($heuredebutreservation)<=3)
+			$heuredebutreservation="0".$heuredebutreservation;
+
+			$anneereservation	= substr($datereservation,0,4);
+			$moisreservation	= substr($datereservation,4,2);
+			$jourreservation	= substr($datereservation,6,2);
+			$heurereservation	= substr($heuredebutreservation,0,2);
+			$minutereservation	= substr($heuredebutreservation,2,2);
+
+			$dates = gmstrftime("DTSTART:%Y%m%dT%H%M%SZ", mktime($heurereservation, $minutereservation, 0, $moisreservation, $jourreservation, $anneereservation));
+
+			$fichier .= $dates.$newline;
+
+			//date fin reservation
+			$heuredebut = gmstrftime("%H", mktime($heurereservation, $minutereservation, 0, $moisreservation, $jourreservation, $anneereservation));
+			$mindebut 	= gmstrftime("%M", mktime($heurereservation, $minutereservation, 0, $moisreservation, $jourreservation, $anneereservation));
+			$heuredebutenmin = $heuredebut*60 + $mindebut;
+
+			if (strlen($reservation['dureeReservation'])==4)
+			{
+				$heureduree	= substr($reservation['dureeReservation'],0,2);
+				$minduree	= substr($reservation['dureeReservation'],2,2);
+			}
+			if (strlen($reservation['dureeReservation'])==3)
+			{
+				$heureduree	= substr($reservation['dureeReservation'],0,1);
+				$minduree	= substr($reservation['dureeReservation'],1,2);
 
 			}
+			if (strlen($reservation['dureeReservation'])==2)
+			{
+				$heureduree = 0;
+				$minduree = $reservation['dureeReservation'];
+			}
+			$heurefinenmin = $heuredebutenmin+$heureduree*60+$minduree;
+			$heurefin = intval($heurefinenmin/60);
+
+			if (strlen($heurefin)==1)
+			$heurefin="0".$heurefin;
+
+			$minfin=$heurefinenmin%60;
+			if (strlen($minfin)==1)
+			$minfin="0".$minfin;
+
+			$fichier .= "DTEND:".$datereservation."T".$heurefin.$minfin."00Z".$newline;
+
+			//detail de la seance
+			$fichier .= "DESCRIPTION;LANGUAGE=fr-CA:INTITULE : " . $commentaire . " \nDUREE : " . $heureduree . "h" . $minduree . $newline;
+
+			$fichier .= "CATEGORIES:Emplois du temps du PST" . $newline;
+			$fichier .= "DTSTAMP:" . $annee . $mois . $jour . "T" . $heure . $minute . "00Z" . $newline;
+			$fichier .= "UID:" . $annee . $mois . $jour . "T" . "000001Z-" . $i . "@ufrsitec.u-paris10.fr" . $newline;
+			$i = $i+1;
+			$fichier .= "END:VEVENT" . $newline;
 
 		}
 
-		$fichier .= "END:VCALENDAR";
-
-		fin_timer("ICSSALLE");
-		echo afficher_timer("ICSSALLE");
-
-		/*
-		 * Fin du traitement - création du fichier ICS
-		 */
-		$nomfichier=$salle['nom'].".ics";
-		//$nomfichier=ereg_replace("[ ]","_",$nomfichier);
-		//$nomfichier=preg_replace('/\s\s+/',"_",$nomfichier);
-		$nomfichier=str_replace(" ","_",$nomfichier);
-		$nomfichier=strtolower($nomfichier);
-		file_put_contents($nomfichier,$fichier);
-		$uid = $annee.$mois.$jour."T"."000001Z-".$i."@ufrsitec.u-paris10.fr";
-		sendICSFile($nomfichier,$fichier,$SALLE,$uid);
 	}
+
+	$fichier .= "END:VCALENDAR";
+
+	fin_timer("ICSSALLE");
+	echo afficher_timer("ICSSALLE");
+
+	/*
+	* Fin du traitement - création du fichier ICS
+	*/
+	$nomfichier = $salle['nom'].".ics";
+	$nomfichier	= str_replace(" ","_",$nomfichier);
+	$nomfichier	= strtolower($nomfichier);
+	file_put_contents($nomfichier,$fichier);
+
+	$uid = $annee . $mois . $jour . "T" . "000001Z-" . $i . "@ufrsitec.u-paris10.fr";
+	sendICSFile($nomfichier, $fichier, $SALLE, $uid);
+}
 ?>
