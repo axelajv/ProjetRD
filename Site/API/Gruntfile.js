1a392cb22e87a6766b257fd5499458114939ea9b
Indy <dev@indydedeken.fr>
2015-01-12 19:40:57 +0100

    Ajout du package.json grunt

--- Site/API/Gruntfile.js
+++ Site/API/Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
-
     grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
         jsbeautifier : {
             "default": {
                 src : ["../js/*.js", "../css/*.css"]
